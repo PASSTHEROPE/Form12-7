57a7ae6a38527325ca9da97a3803b405709335eb
adi <aditya.bardhan@besquare.com.my>
2022-07-12 13:44:35 +0800

    2dooo

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react"; //wtf is this?
+import List from "./list";
 function App() {
+  const arr: string[] = [];
+  const [currentItem, setCurrentItem] = useState(String);
+  const [itemList, updateItemList] = useState<string[]>([]); //have to define array as string or gg cuz default never or some shit
+  const onChangeHandler = (e: {
+    target: { value: React.SetStateAction<string> }; //quick fix but idk
+  }) => {
+    console.log("current value", e.target.value);
+    setCurrentItem(e.target.value);
+  };
+
+  const addItemToList = () => {
+    updateItemList([...itemList, currentItem]);
+    setCurrentItem(""); //clears
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="Wrapper">
+          <div className="input-wrapper">
+            <input value={currentItem} onChange={onChangeHandler} />
+            <button onClick={addItemToList}>ADD</button>
+          </div>
+          <List />
+        </div>
       </header>
     </div>
   );
